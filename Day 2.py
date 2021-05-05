b75240ec3cc162fed836e9d58ecd94f988aae241
SonyThangam <83686324+SonyThangam@users.noreply.github.com>
2021-05-05 10:57:31 +0530

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,43 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # int datatype
+>>> num = 5
+>>> type(num)
+<class 'int'>
+>>> #float datatype
+>>> num = 7.2
+>>> type(num)
+<class 'float'>
+>>> #string datatype
+>>> num = " python "
+>>> type(num)
+<class 'str'>
+>>> #bool datatype
+>>> num = 2+8
+>>> type(num)
+<class 'int'>
+>>> num = 2+7
+>>> bool(num)
+True
+>>> #complex datatype
+>>> a = 6
+>>> b = 7
+>>> c = complex(a,b)
+>>> print(c)
+(6+7j)
+>>> num = 6+7j
+>>> type(num)
+<class 'complex'>
+>>> #int to float
+>>> a = 3
+>>> float(a)
+3.0
+>>> #float to int
+>>> b = 7.4
+>>> int(b)
+7
+>>> #str() with values
+>>> a = str("python")
+>>> print(a)
+python
+>>> 
