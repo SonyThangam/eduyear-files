db85ff5c03a3ca49ea965de0a42d7e50dfd48e3c
SonyThangam <83686324+SonyThangam@users.noreply.github.com>
2021-05-06 09:53:26 +0530

    Add files via upload

---
+++ Day 3 Age calculator.py
@@ -0,0 +1,9 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Age calculator
+>>> birthyear = int(input("Enter your Birthyear :  "))
+Enter your Birthyear :  1990
+>>> age = 2021 - birthyear
+>>> print("Your age is",age,"years old")
+Your age is 31 years old
+>>> 
---
+++ Day 3 Arithmetic operators.py
@@ -0,0 +1,19 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #arithmetic operators
+>>> a , b = 10 , 5
+>>> print("a + b = {}". format(a + b))
+a + b = 15
+>>> print("a - b = {}". format(a - b))
+a - b = 5
+>>> print("a * b = {}". format(a * b))
+a * b = 50
+>>> print("a / b = {}". format(a / b))
+a / b = 2.0
+>>> print("a % b = {}". format(a % b))
+a % b = 0
+>>> print("a // b = {}". format(a // b))
+a // b = 2
+>>> print("a ** b = {}". format(a ** b))
+a ** b = 100000
+>>> 
